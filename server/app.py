530504a0853c1331f58953e9772c79442af1e4f6
Arnold-In-Tech <g16a7782@gmail.com>
2024-04-09 05:26:11 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,92 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method == 'GET':
+        messages = []
+        from sqlalchemy import asc
+        for message in Message.query.order_by(asc('created_at')).all(): # ordered by created_at in ascending order.
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages,
+            200
+        )
+
+        return response
+
+
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.form.get("body"),
+            username=request.form.get("username")
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+        
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+        return response
+
+        
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response(response_body, 404)
+
+        return response
+
+
+    else:
+        if request.method == 'PATCH':
+        
+            for attr in request.form:
+                setattr(message, attr, request.form.get(attr))
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+            
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+
+ 
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."
+            }
+
+            response = make_response(
+                response_body,
+                204
+            )
+
+            return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
